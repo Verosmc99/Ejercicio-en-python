a841e3099d51505fd78c84fefa5597059f41ca32
Verosmc99 <veronica.martinez@ute.edu.ec>
2022-07-13 23:31:11 -0500

    first commit

---
+++ broke.py
@@ -0,0 +1,13 @@
+def f():
+    broken_code
+    print('fuckit chaining works')
+
+for
+
+let's just assume this is a big module of shitty code.
+
+x = y
+y = x
+1 / 0 # Oh shhhiiiiiii
+
+var = "Are you proud of what you've done?"
